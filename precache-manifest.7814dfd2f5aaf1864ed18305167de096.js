ce20acfda91ff0a1aae61c0b6dbb3feaeb0a2b8c
RomanCox <rom.cox@gmail.com>
2022-03-05 18:24:57 +0300

    Updates

---
+++ precache-manifest.7814dfd2f5aaf1864ed18305167de096.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7cb1e02fa792cf1481878972f707d5f",
+    "url": "/homeworks/index.html"
+  },
+  {
+    "revision": "c16d54d44a21e5c5cf90",
+    "url": "/homeworks/static/css/main.404c83f1.chunk.css"
+  },
+  {
+    "revision": "a5eec3b31ca7866aef3a",
+    "url": "/homeworks/static/js/2.490fbe74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/homeworks/static/js/2.490fbe74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c16d54d44a21e5c5cf90",
+    "url": "/homeworks/static/js/main.2e94ed0c.chunk.js"
+  },
+  {
+    "revision": "9263b9c20382da27b976",
+    "url": "/homeworks/static/js/runtime-main.1f7d4fd3.js"
+  }
+]);
