b047b52170c892ae3c3ea20794a6e67be378a037
RomanCox <rom.cox@gmail.com>
2022-03-11 18:51:03 +0300

    First HW5 Working Commit

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import {NavLink} from "react-router-dom";
+import {PATH} from "./Navigation";
 
 function Header() {
     return (
         <div>
-            // add NavLinks
-
+            <NavLink to={PATH.PRE_JUNIOR}>PRE-JUNIOR</NavLink>
+            <NavLink to={PATH.JUNIOR}>JUNIOR</NavLink>
+            <NavLink to={PATH.JUNIOR_PLUS}>JUNIOR+</NavLink>
         </div>
     )
 }
---
+++ src/p2-homeworks/h5/Navigation.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import {Navigate, Routes, Route} from "react-router-dom";
+import PreJunior from "./pages/PreJunior";
+import Error404 from "./pages/Error404";
+import Junior from "./pages/Junior";
+import JuniorPlus from "./pages/JuniorPlus";
+
+export const PATH = {
+    PRE_JUNIOR: '/pre-junior',
+    JUNIOR: '/junior',
+    JUNIOR_PLUS: '/junior-plus',
+}
+
+function Navigation() {
+    return (
+        <div>
+            {/*Switch выбирает первый подходящий route*/}
+            <Routes>
+
+            {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу PRE_JUNIOR*/}
+            {/*exact нужен, чтобы указать полное совпадение (что после '/' ничего не будет)*/}
+                <Route path={'/'} element={<Navigate to={PATH.PRE_JUNIOR}/>}/>
+
+                <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
+                <Route path={PATH.JUNIOR} element={<Junior/>}/>
+                <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>}/>
+                {/*у этого route нет пути, он отрисуется, если пользователь захочет попасть на несуществующую страницу*/}
+                <Route path='*' element={<Error404/>}/>
+
+            </Routes>
+        </div>
+    )
+}
+
+export default Navigation
