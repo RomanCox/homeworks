c17e4c2de3b277f292a93abbc4eb4d4f530d7177
RomanCox <rom.cox@gmail.com>
2022-03-19 13:22:56 +0300

    Final HW9 without Alternative Commit

--- src/p2-homeworks/h9/HW9.tsx
+++ src/p2-homeworks/h9/HW9.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Clock from './Clock'
+import s from './HW9.module.css'
 
 function HW9() {
     return (
-        <div>
-            <hr/>
-            homeworks 9
-
+        <div className={s.container}>
+            <div className={s.h2Container}>
+                <h2>homeworks 9</h2>
+            </div>
             {/*should work (должно работать)*/}
             <Clock/>
 
